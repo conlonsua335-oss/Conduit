0d57d001a01a06028d501b528552910838a5e2df
Tuan Anh <conlonsua335@gmail.com>
2026-05-18 22:03:30 +0700

    fix duplicate RegisterPage

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { parseApiError, registerApi } from "../api/auth";
-
+import { registerFields } from "../components/form/authFields";
+import AuthForm from "../components/form/AuthForm";
 function RegisterPage() {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async () => {
     setError("");
 
-    if (!username || !email || !password) {
+    if (!form.username || !form.email || !form.password) {
       setError("Vui lòng nhập đầy đủ thông tin")
       return
     }
     setIsLoading(true);
 
     try {
-      await registerApi(username, email, password);
+      await registerApi(form.username, form.email, form.password);
       navigate("/login");
     } catch (err: unknown) {
       console.log("Lỗi:", err);
@@ -46,36 +50,15 @@ function RegisterPage() {
         </div>
       )}
 
-      <div className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="border border-gray-300 rounded px-4 py-3 w-full"
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border border-gray-300 rounded px-4 py-3 w-full"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border border-gray-300 rounded px-4 py-3 w-full"
-        />
-        <button
-          onClick={handleSubmit}
-          disabled={isLoading}
-          className="bg-green-500 text-white px-6 py-3 rounded text-lg self-end hover:bg-green-600 disabled:opacity-50"
-        >
-          {isLoading ? "Đang xử lý..." : "Sign up"}
-        </button>
-      </div>
+      <AuthForm
+        fields={registerFields}
+        form={form}
+        setForm={setForm}
+        onSubmit={handleSubmit}
+        isLoading={isLoading}
+        buttonText="Sign up"
+      />
+
     </div>
   );
 }
